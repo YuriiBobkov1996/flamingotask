14c029579690c309428181cdf8a2c40e7dc07221
YuriiBobkov1996 <yuriibobkov1996@gmail.com>
2026-05-14 15:39:48 +0100

    added ApiUiIntegrationTest for filling form in DemoQA

---
+++ src/test/java/integration/ApiUiIntegrationTest.java
@@ -0,0 +1,43 @@
+package integration;
+
+import api.rest.helpers.ApiHelper;
+import api.rest.models.BookingRequest;
+import api.rest.models.BookingResponse;
+import api.rest.testdata.BookingTestData;
+import org.junit.jupiter.api.Test;
+import ui.base.BaseUITest;
+import ui.pages.FormPage;
+import ui.utils.BirthDate;
+import ui.utils.FormTestUtils;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class ApiUiIntegrationTest extends BaseUITest {
+    private final ApiHelper apiHelper = new ApiHelper();
+
+    @Test
+    void shouldCreateBookingViaApiAndSubmitUiFormWithBookingData() {
+        BookingRequest bookingToCreate = BookingTestData.defaultBooking();
+        BookingResponse createdBooking = apiHelper.createBooking(bookingToCreate);
+        BookingRequest retrievedBooking = apiHelper.getBookingById(createdBooking.getBookingid());
+        String firstName = retrievedBooking.getFirstname();
+        String lastName = retrievedBooking.getLastname();
+        String email = faker.internet().emailAddress();
+        String mobile = faker.number().digits(10);
+        String address = faker.address().streetAddress();
+        BirthDate birthDate = FormTestUtils.getRandomBirthDate(faker);
+        FormPage formPage = new FormPage(page);
+        formPage.open();
+        formPage.fillRequiredFields(firstName, lastName, email, mobile);
+        formPage.selectDateOfBirth(birthDate.getMonth(), birthDate.getYear(), birthDate.getDay());
+        formPage.selectSubject("Maths");
+        formPage.selectSportsHobby();
+        formPage.uploadPicture(FormTestUtils.getTestImage());
+        formPage.fillAddress(address);
+        formPage.selectState();
+        formPage.selectCity();
+        formPage.clickSubmitButton();
+        assertThat(formPage.successModal()).isVisible();
+        assertThat(formPage.successModalTitle()).hasText("Thanks for submitting the form");
+    }
+}
